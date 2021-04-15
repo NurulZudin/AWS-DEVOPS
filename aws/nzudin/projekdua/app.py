86f3ea11f4a49ecd55d7747890cb27bb8c0de5ad
nurulzudin <nurulzudin18@gmail.com>
2021-04-15 23:14:56 +0800

    fix the error

--- aws/nzudin/projekdua/app.py
+++ aws/nzudin/projekdua/app.py
@@ -7,37 +7,20 @@ app=Flask(__name__)
 def index():
         return render_template('index.html')
 
-def Convert_Roman( num):
-        symbol = [ "M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV","I"]
-        value = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
-        roman_num = ''
-        i = 0
-        while  num > 0:
-            for x in range(num // value[i]):
-                roman_num += symbol[i]
-            num %= value[i]
-            i += 1
-        return roman_num
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
 
-print("###This program converts decimal numbers to Roman Numerals ###")
-print('(To exit the program, please type "exit" )')
+def num2roman(num):
 
+    roman = ''
 
-condition=True
-while condition :
-    try:
-        Input_Text=input("Please enter a number between 1 and 3999, inclusively : ")
-        Number = int(Input_Text )
-        if 4000>Number>0 :
-            print(Convert_Roman(Number))
-        else:
-            print("Not Valid Input !!!")
-    except ValueError:
-        if Input_Text.lower() == "exit":
-            condition=False
-            print("Exiting the program... Good Bye")
-        else:
-            print("Not Valid Input !!!")
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    return roman        
 
 @app.route('/total',methods=["GET","POST"])
 def total():
